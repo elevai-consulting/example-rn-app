66caedde291cf083d89eb2735f593ef027f17509
Dan Rivett <dan.rivett@elevai.com>
2020-08-06 20:30:39 -0700

    (aws-amplify/amplify-js#6453) Initial commit for attempting to recreate https://github.com/aws-amplify/amplify-js/issues/6453. Not currently successful.

--- App.tsx
+++ App.tsx
@@ -1,7 +1,11 @@
 import Amplify from '@aws-amplify/core';
+import { DataStore } from '@aws-amplify/datastore';
 import { AppLoading } from 'expo';
 import React, { useState } from 'react';
-import { Text, View } from 'react-native';
+import { Button, Text, View } from 'react-native';
+import { TextInput } from 'react-native-paper';
+
+import { Movie } from '~models';
 
 import awsconfig from './src/aws-exports';
 import styles from './styles';
@@ -12,6 +16,40 @@ const initializeApp = async () => {};
 
 const App: React.FC = () => {
   const [isInitialized, setIsInitialized] = useState(false);
+  const [movieName, setMovieName] = useState<string>();
+  const [movieDescription, setMovieDescription] = useState<string>();
+  const [result, setResult] = useState<string>();
+
+  const saveMovie = async () => {
+    try {
+      if (movieDescription) {
+        console.log(`Saving Movie '${movieName}' with description: ${movieDescription}`);
+
+        await DataStore.save(
+          new Movie({
+            name: movieName,
+            description: movieDescription,
+          })
+        );
+      } else {
+        console.log(`Saving Movie '${movieName}' without description.`);
+
+        await DataStore.save(
+          new Movie({
+            name: movieName,
+          })
+        );
+      }
+
+      setResult('Movie saved locally in DataStore');
+    } catch (error) {
+      setResult(`Failed to save movie locally in DataStore: ${error}`);
+    }
+  };
+
+  const clearResult = () => {
+    setResult(null);
+  };
 
   if (!isInitialized) {
     return (
@@ -25,7 +63,31 @@ const App: React.FC = () => {
 
   return (
     <View style={styles.screen}>
-      <Text style={styles.heading}>{'Demo App'}</Text>
+      <Text style={styles.heading}>{'Demo: aws-amplify/amplify-js #6453'}</Text>
+      <View style={styles.fieldRow}>
+        <View style={styles.fieldLabel}>
+          <Text style={styles.fieldLabelText}>{'Movie Name'}</Text>
+        </View>
+        <TextInput
+          value={movieName}
+          onChangeText={setMovieName}
+          onFocus={clearResult}
+          style={styles.fieldInput}
+        />
+      </View>
+      <View style={styles.fieldRow}>
+        <View style={styles.fieldLabel}>
+          <Text style={styles.fieldLabelText}>{'Movie Description'}</Text>
+        </View>
+        <TextInput
+          value={movieDescription}
+          onChangeText={setMovieDescription}
+          onFocus={clearResult}
+          style={styles.fieldInput}
+        />
+      </View>
+      <Button title="Press to Save" onPress={saveMovie} />
+      <View style={styles.result}>{result && <Text style={styles.resultText}>{result}</Text>}</View>
     </View>
   );
 };
--- styles.ts
+++ styles.ts
@@ -2,6 +2,7 @@ import { StyleSheet } from 'react-native';
 
 const BASE_FONT_SIZE = 12;
 const H1_FONT_SIZE = 1.5 * BASE_FONT_SIZE;
+const RESULT_SIZE = 1.5 * BASE_FONT_SIZE;
 
 export default StyleSheet.create({
   screen: {
@@ -14,5 +15,29 @@ export default StyleSheet.create({
     textAlign: 'center',
     width: '100%',
     marginBottom: 50,
+  },
+  fieldRow: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    margin: 20,
+  },
+  fieldLabel: {
+    flex: 1,
+    marginRight: 20,
+  },
+  fieldLabelText: {
+    fontWeight: 'bold',
+  },
+  fieldInput: {
+    width: 200,
+  },
+  result: {
+    marginTop: 50,
+    width: '100%',
+  },
+  resultText: {
+    fontSize: RESULT_SIZE,
+    fontWeight: 'bold',
+    textAlign: 'center',
   },
 });
